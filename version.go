c11e03809be6617e1b35d1712ae5128d46e58d5b
7sunarni <710720732@qq.com>
2024-11-25 15:19:13 +0800

    add log

--- version.go
+++ version.go
@@ -1,5 +1,7 @@
 package version
 
+import "log"
+
 var (
 	GitMajor string
 	GitMinor string
@@ -10,3 +12,7 @@ var (
 
 	BuildDate = "1970-01-01T00:00:00Z"
 )
+
+func init() {
+	log.Printf("GitVersion: %s GitCommit: %s", GitVersion, GitCommit)
+}
