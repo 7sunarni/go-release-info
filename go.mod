fb53c0c6bc442e202e376401e483a3517d12e7c7
7sunarni <710720732@qq.com>
2024-11-25 15:07:28 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/7sunarni/go-release-info
+
+go 1.22.5
---
+++ version.go
@@ -0,0 +1,12 @@
+package version
+
+var (
+	GitMajor string
+	GitMinor string
+
+	GitVersion   = "v0.0.0-master+$Format:%H$"
+	GitCommit    = "$Format:%H$"
+	GitTreeState = ""
+
+	BuildDate = "1970-01-01T00:00:00Z"
+)
